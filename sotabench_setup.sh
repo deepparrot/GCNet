9827a7543b66f84ab2d5a932b9882eb78f7af84d
deepparrot <53057954+deepparrot@users.noreply.github.com>
2019-09-30 17:26:45 +0100

    Add Apex

--- sotabench_setup.sh
+++ sotabench_setup.sh
@@ -2,6 +2,11 @@
 source /workspace/venv/bin/activate
 PYTHON=${PYTHON:-"python"}
 
+apt-get git
+git clone https://github.com/NVIDIA/apex
+cd apex
+pip install -v --no-cache-dir --global-option="--cpp_ext" --global-option="--cuda_ext" ./
+
 pip install torch
 
 echo "Building roi align op..."
